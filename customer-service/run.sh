850626a6061f05c0fe9236a6588f33c983fec539
Benjamin Wilms <benjamin.wilms@codecentric.de>
2016-04-14 19:31:59 +0200

    Docker config, Spring Boot Ports changed

--- customer-service/run.sh
+++ customer-service/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo check etcd service status
+while ! curl http://192.168.99.100:2379/v2/keys/hystrix
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+echo "$(date) - connected successfully"
+
 set -e
 
 if [ "${1:0:1}" != '-' ]; then
