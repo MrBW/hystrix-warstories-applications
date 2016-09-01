d029413985c2a9abad890afd996e59d325c84bd5
Benjamin Wilms <benjamin.wilms@codecentric.de>
2016-09-01 10:25:49 +0200

    Fixed docker issue

--- presentation-steps/docker-env/step_2.sh
+++ presentation-steps/docker-env/step_2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-baseurl=http://192.168.99.100:2379/v2/keys/hystrix
+baseurl=http://127.0.0.1:2379/v2/keys/hystrix
 
 # Default Service URLs
 clear
 echo +++ Step 2 - Connect Remote Services +++
 echo
-curl -L -X PUT $baseurl/service.address.customer -d value="http://customer-service:8080/customer/find/accountnumber/"
+echo service.address.customer = http://customer-service/customer/find/accountnumber/
+echo service.address.connote  = http://connote-service/connote/create/
 echo
-curl -L -X PUT $baseurl/service.address.connote -d value="http://connote-service:8080/connote/create/"
+curl -L -X PUT $baseurl/service.address.customer -d value="http://customer-service/customer/find/accountnumber/"
+echo
+curl -L -X PUT $baseurl/service.address.connote -d value="http://connote-service/connote/create/"
 echo
